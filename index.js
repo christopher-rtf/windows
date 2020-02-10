89d597899191b9ac0b9d88f84058a9b57139d7ef
Christopher - RtF <5820035+christopher-rtf@users.noreply.github.com>
2020-02-10 17:27:43 -0400

    xplat: Recreated repo base for VS2017/Electron 6.0.8 xplat testing

--- index.js
+++ index.js
@@ -15,40 +15,6 @@ https://github.com/gpii/universal/LICENSE.txt
 
 "use strict";
 
-var fluid = require("gpii-universal"),
-    path = require("path");
-
-fluid.module.register("gpii-windows", __dirname, require);
-
-fluid.contextAware.makeChecks({
-    "gpii.contexts.windows": {
-        value: true
-    }
-});
-
-// Add ./bin to the path.
-var binPath = path.join(__dirname, "bin");
-process.env.path = binPath + ";" + process.env.path;
-
-require("./gpii/node_modules/WindowsUtilities/WindowsUtilities.js");
-require("./gpii/node_modules/processHandling/processHandling.js");
-require("./gpii/node_modules/displaySettingsHandler");
-require("./gpii/node_modules/registrySettingsHandler");
-require("./gpii/node_modules/registryResolver");
-require("./gpii/node_modules/spiSettingsHandler");
-require("./gpii/node_modules/registeredAT/registeredAT.js");
-require("./gpii/node_modules/windowsMetrics");
-require("./gpii/node_modules/processReporter");
-require("./gpii/node_modules/windowMessages");
-require("./gpii/node_modules/userListeners");
-require("./gpii/node_modules/systemSettingsHandler");
-require("./gpii/node_modules/nativeSettingsHandler");
-require("./gpii/node_modules/gpii-app-zoom");
-require("./gpii/node_modules/wmiSettingsHandler");
-require("./gpii/node_modules/gpii-localisation");
-require("./gpii/node_modules/gpii-office");
-require("./gpii/node_modules/gpii-userInput");
-require("./gpii/node_modules/gpii-service-handler");
-require("./gpii/node_modules/gpii-windowManagement");
+var fluid = require("gpii-universal");
 
 module.exports = fluid;
