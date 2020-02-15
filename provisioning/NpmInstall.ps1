4b093c537efd75cb1b1f83744ce3d528b6e964ee
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2020-02-15 19:31:03 -0400

    GPII-4356: Updated VolumeControl build to support both x86 and x64 architectures using VS2017

---
+++ provisioning/NpmInstall.ps1
@@ -0,0 +1,72 @@
+param([String]$arch)
+<#
+  This script is executed by `npm install`, and builds things required by gpii-windows.
+#>
+# Turn verbose on, change to "SilentlyContinue" for default behaviour.
+$VerbosePreference = "continue"
+
+if (!$arch) {
+    throw "Argument '-arch' (architecture) must be passed to this script.  Valid options are: [x86, x64, arm64]"
+}
+
+switch ($arch) {
+  "x86" {
+    echo "Building Windows executables for Intel 32 (i.e. IA32/x86) architecture"
+    $targetArchitecture = "x86"
+  }
+  "x64" {
+    echo "Building Windows executables for Intel 64 (i.e. AMD64/x64/x86_64) architecture"
+    $targetArchitecture = "x64"
+  }
+  "arm64" {
+    echo "Building Windows executables for ARM64 architecture"
+    $targetArchitecture = "ARM64"
+  }
+  default {
+    throw "Invalid architecture '$($arch)'.  Valid options are: [x86, x64, arm64]"
+  }
+}
+
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$rootDir = Split-Path -Parent $scriptDir
+
+if (${Env:ProgramFiles(x86)}) {
+  $programFilesX86Path = ${Env:ProgramFiles(x86)}
+} else {
+  # This is required for compatibility with Intel 32-bit systems (since the ProgramFiles(x86) environment variable does not exist on 32-bit Windows)
+  $programFilesX86Path = ${Env:ProgramFiles}
+}
+$programFilesPath = ${Env:ProgramFiles}
+
+# Include main Provisioning module.
+Import-Module (Join-Path $scriptDir 'Provisioning.psm1') -Force -Verbose
+
+# Capture the full file path of MSBuild; we will accept any version >=15.0 and <16.0 (i.e. VS2017)
+$msbuild = Get-MSBuild "[15.0,16.0)"
+
+# # Build the settings helper
+# $settingsHelperDir = Join-Path $rootDir "settingsHelper"
+# Invoke-Command $msbuild "SettingsHelper.sln /p:Configuration=Release /p:Platform=`"Any CPU`" /p:FrameworkPathOverride=`"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $settingsHelperDir
+
+$volumeControlDir = Join-Path $rootDir "gpii\node_modules\nativeSettingsHandler\nativeSolutions\VolumeControl"
+switch ($targetArchitecture) {
+  "x86" {
+    Invoke-Command $msbuild "VolumeControl.sln /p:Configuration=Release /p:Platform=`"$($targetArchitecture)`" /p:FrameworkPathOverride=`"$($programFilesX86Path)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $volumeControlDir
+  }
+  "x64" {
+    Invoke-Command $msbuild "VolumeControl.sln /p:Configuration=Release /p:Platform=`"$($targetArchitecture)`" /p:FrameworkPathOverride=`"$($programFilesPath)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $volumeControlDir
+  }
+  "ARM64" {
+    # NOTE: the VolumeControl project uses the Windows 8.1 SDK, so it's not possible to compile for ARM64.  Therefore we compile for x86 instead (and use x86 on ARM64 emulation)
+    Invoke-Command $msbuild "VolumeControl.sln /p:Configuration=Release /p:Platform=`"$($targetArchitecture)`" /p:FrameworkPathOverride=`"$($programFilesX86Path)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $volumeControlDir
+  }
+}
+
+# # Build the process test helper
+# $testProcessHandlingDir = Join-Path $rootDir "gpii\node_modules\processHandling\test"
+# $csc = Join-Path -Path (Split-Path -Parent $msbuild) csc.exe
+# Invoke-Command $csc "/target:exe /out:test-window.exe test-window.cs" $testProcessHandlingDir
+
+# # Build the Windows Service
+# $serviceDir = Join-Path $rootDir "gpii-service"
+# Invoke-Command "npm" "install" $serviceDir
---
+++ provisioning/Provisioning.psm1
@@ -0,0 +1,127 @@
+<#
+  This script execute all the needed steps to build all the Windows GPII
+  components.
+
+  Requirements:
+    * Windows PowerShell 3.0 or greater.
+    * PowerShell Core
+#>
+
+$VerbosePreference = "continue"
+
+Function Invoke-Command {
+  Param (
+    [string] $command,
+    [string] $arguments,
+    [string] $location,
+    [int] $errorLevel = 0
+  )
+
+  Write-Verbose "Execute-Command Started ($location : $command $arguments)."
+
+  if (!$command) {
+    throw "You need to specify a command to Execute-Command."
+  }
+  if (!$arguments) {
+    $arguments = " "
+  }
+  $originalLocation = Get-Location
+  if ($location) {
+    Set-Location -Path $location
+  }
+
+  $p = Start-Process -FilePath $command -ArgumentList $arguments -PassThru -NoNewWindow
+  $handle = $p.Handle
+  $p.WaitForExit();
+
+  $exitCode = $p.ExitCode
+
+  Write-Verbose "`'$command`' exited with $($exitCode)"
+
+  Set-Location -Path $originalLocation.path
+
+  if ($exitCode -gt $errorLevel) {
+    throw "Installation process returned error code: $($exitCode)"
+  }
+}
+
+Function Get-MSBuild {
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string] $visualStudioVersion
+  )
+
+  # Valid Visual Studio versions options are [15.0, "[15.0,16.0)", etc.]
+
+  # Find the path vswhere (which installed with Visual Studio 2017 or Build Tools 2017 or later)
+  if (${Env:ProgramFiles(x86)}) {
+    $vswhere = Join-Path ${Env:ProgramFiles(x86)} "Microsoft Visual Studio\Installer\vswhere.exe"
+  } else {
+    $vswhere = Join-Path ${Env:ProgramFiles} "Microsoft Visual Studio\Installer\vswhere.exe"
+  }
+
+  if (!$vswhere) {
+    throw "Visual Studio $($visualStudioVersion) or Build Tools $($visualStudioVersion) were not found."
+  }
+
+  $path = &$vswhere -version $visualStudioVersion -products * -requires Microsoft.Component.MSBuild -property installationPath
+  if ($path) {
+    $msbuild = Join-Path $path 'MSBuild\Current\Bin\MSBuild.exe'
+    if (-not (test-path $msbuild)) {
+      $msbuild = join-path $path 'MSBuild\15.0\Bin\MSBuild.exe'
+      if (-not (test-path $msbuild)) {
+        throw "MSBuild from Visual Studio $($visualStudioVersion) or Build Tools $($visualStudioVersion) could not be found"
+      }
+    }
+  }
+
+  return $msbuild
+}
+
+# Function Invoke-Environment {
+#   Param (
+#         [Parameter(Mandatory=$true)]
+#         [string] $Command
+#     )
+# 
+#     $Command = "`"" + $Command + "`""
+#     cmd /c "$Command > nul 2>&1 && set" | . { process {
+#         if ($_ -match '^([^=]+)=(.*)') {
+#             [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+#         }
+#     }}
+# }
+
+# # TODO: Currently this function is adding the directory location even if this
+# # is present in the path. Implement an additional function to check existence.
+# Function Add-Path {
+#   Param (
+#     [Parameter(Mandatory=$true)]
+#     [string] $Directory,
+#     [bool] $Permanent = $false
+#   )
+# 
+#   $regKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+#   $normalizedDirectory = [System.IO.Path]::GetFullPath($Directory)
+# 
+#   # Obtain and set local PATH.
+#   $currentLocalPath = $env:Path
+#   $newLocalPath = "$($currentLocalPath);$($normalizedDirectory)"
+#   $env:Path = $newLocalPath
+#   Write-Verbose "Add-Path Local $($normalizedDirectory) -> $($newLocalPath)"
+# 
+#   if ($Permanent) {
+#     # Obtain and set system PATH.
+#     $currentSystemPath = (Get-ItemProperty -Path $regKey -Name PATH).Path
+#     $newSystemPath = "$($currentSystemPath);$($normalizedDirectory)"
+#     Write-Verbose "Add-Path System $($normalizedDirectory) in the registry: $($newSystemPath)."
+#     Set-ItemProperty -Path $regKey -Value $newSystemPath -Name PATH
+#   }
+# 
+#   return $newLocalPath
+# }
+
+Export-ModuleMember -Function Invoke-Command
+Export-ModuleMember -Function Get-MSBuild
+# Export-ModuleMember -Function Invoke-Environment
+# Export-ModuleMember -Function Add-Path
